3f9d1739dfcca43bc0db11ce0ac728f0bd3c0392
beader <beader@qq.com>
2026-04-06 22:22:42 +0800

    update repo name

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/penang-bus/',
+  base: '/rapidpenang/',
   plugins: [react(), tailwindcss()],
 })
